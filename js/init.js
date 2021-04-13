32c56fcf7ecca8a088ac218c74772abc84325fb5
davidurban1 <74219572+davidurban1@users.noreply.github.com>
2021-04-13 03:00:42 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
 
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyCRlRGrCLsLr6Qs8GuuwI0DGZ94PlmBhrQ",
-    authDomain: "davidurban5738.firebaseapp.com",
-    projectId: "davidurban5738",
-    storageBucket: "davidurban5738.appspot.com",
-    messagingSenderId: "286467288987",
-    appId: "1:286467288987:web:595ad1fa19d446463bbb3e"
+   apiKey: "AIzaSyBCHZIbuMATmEIUqNk2dDc7fAt_dR16w3Q",
+    authDomain: "david57.firebaseapp.com",
+    projectId: "david57",
+    storageBucket: "david57.appspot.com",
+    messagingSenderId: "669989599299",
+    appId: "1:669989599299:web:3facc18a54f62a11f43cf6"
  
 });
