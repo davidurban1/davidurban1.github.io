f7f131eebe2ca6ace842102b34d8f163ecaea36c
davidurban1 <74219572+davidurban1@users.noreply.github.com>
2021-04-12 20:02:18 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+   apiKey: "AIzaSyCRlRGrCLsLr6Qs8GuuwI0DGZ94PlmBhrQ",
+    authDomain: "davidurban5738.firebaseapp.com",
+    projectId: "davidurban5738",
+    storageBucket: "davidurban5738.appspot.com",
+    messagingSenderId: "286467288987",
+    appId: "1:286467288987:web:595ad1fa19d446463bbb3e"
 });
