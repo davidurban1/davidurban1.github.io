04316d603067b9ba143332683f6dc079414a4fdd
davidurban1 <74219572+davidurban1@users.noreply.github.com>
2021-04-12 21:29:02 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,14 @@
 // @ts-ignore
-firebase.initializeApp({
-   apiKey: "AIzaSyCRlRGrCLsLr6Qs8GuuwI0DGZ94PlmBhrQ",
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCRlRGrCLsLr6Qs8GuuwI0DGZ94PlmBhrQ",
     authDomain: "davidurban5738.firebaseapp.com",
     projectId: "davidurban5738",
     storageBucket: "davidurban5738.appspot.com",
     messagingSenderId: "286467288987",
     appId: "1:286467288987:web:595ad1fa19d446463bbb3e"
-});
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
