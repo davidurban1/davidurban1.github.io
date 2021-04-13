6d66d428e1eb456b1e6910f811bdcb9d90c72990
davidurban1 <74219572+davidurban1@users.noreply.github.com>
2021-04-12 20:55:21 -0500

    Delete js directory

--- js/init.js
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-firebase.initializeApp({
-   apiKey: "AIzaSyCRlRGrCLsLr6Qs8GuuwI0DGZ94PlmBhrQ",
-    authDomain: "davidurban5738.firebaseapp.com",
-    projectId: "davidurban5738",
-    storageBucket: "davidurban5738.appspot.com",
-    messagingSenderId: "286467288987",
-    appId: "1:286467288987:web:595ad1fa19d446463bbb3e"
-});
